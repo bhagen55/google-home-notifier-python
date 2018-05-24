8acc168e133582e8cd19d695388a28a6a2294d19
techhouse <bhagen55@gmail.com>
2018-05-24 18:21:59 -0400

    Continuing work on volume control

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from gtts import gTTS
 from slugify import slugify
 from pathlib import Path
 from urllib.parse import urlparse
+from mutagen.mp3 import MP3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-chromecast_name = "Techhouse" #edit me to be your google home group
-path = "/static/cache/"
+chromecast_name = "Blair" #edit me to be your google home group
+path = "./static/cache/"
 
 app = Flask(__name__)
 logging.info("Starting up chromecasts")
@@ -63,18 +64,18 @@ def force_play(choice):
 @app.route('/vol/<level>')
 def set_vol(level):
     global vol_level
-    level = int(level)
+    level = float(level)
     if level <= 1 and level >= 0:
         vol_level = level
         return "vox volume set to " + str(vol_level)
     else:
         return "invalid input, please give a value between 0 and 1"
 
-def play_mp3(mp3_url):
+def play_mp3(mp3):
     cast.wait()
     mc = cast.media_controller
-    mc.play_media(mp3_url, 'audio/mp3')
-
+    print("Playing " + str(mp3))
+    mc.play_media(mp3, 'audio/mp3')
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -84,16 +85,20 @@ def send_static(path):
 @app.route('/play/<filename>')
 def play(filename):
     urlparts = urlparse(request.url)
-    mp3 = Path("./static/cache/"+filename)
-    print(mp3.is_file())
+    mp3 = Path("./" + filename)
+    print("Combined MP3 path is: " + str(mp3))
     if mp3.is_file():
         if cast.is_idle or force_cast == True:
             old_vol = cast.status.volume_level
             cast.set_volume(vol_level)
-            print("setting volume to " + vol_level)
-            play_mp3(mp3)
+            print("setting volume to " + str(vol_level))
+            play_mp3("http://"+urlparts.netloc+"/"+str(mp3))
+            mp3_file = MP3("./" + str(mp3))
+            st_time = time.time()
+            #while time.time() - st_time < mp3_file.info.length:
+            #    print("loop")
             cast.set_volume(old_vol)
-            print("Returning volume to " + old_vol)
+            print("Returning volume to " + str(old_vol))
             return filename
         else:
             return "Device Is Busy"
@@ -117,11 +122,12 @@ def sayvox(text):
         return False
     else:
         filename = get_vox_mp3(text)
-        print(filename)
+        print("Generated filename is: " + str(filename))
         if filename is None:
             return "Vox can't say any of those words"
         else:
-            return "Vox says: " + play(filename)
+            play(filename)
+            return ("Vox says: " + filename)
 
 def get_vox_mp3(text):
     return vox.savetomp3(text)
